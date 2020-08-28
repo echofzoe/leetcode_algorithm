02ccec55771317610cd575d75ac2412c53e5f324
echofzoe <echofzoe@gmail.com>
2020-08-28 17:28:37 +0800

    2020-08-28 afternoon

---
+++ src/algorithm/testdemo/testdemo.java
@@ -0,0 +1,18 @@
+package algorithm.testdemo;
+
+import org.junit.Test;
+
+public class testdemo {
+
+    @Test
+    public void test1() {
+        int a = -2;
+        String bin_a = Integer.toBinaryString(a);
+        System.out.println(bin_a);
+
+        a = a << 31;
+        bin_a = Integer.toBinaryString(a);
+        System.out.println(bin_a);
+    }
+
+}
