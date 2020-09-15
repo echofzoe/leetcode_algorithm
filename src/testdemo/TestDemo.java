d58d80acd58905cfe1974b129d112d83a322464b
echofzoe <echofzoe@gmail.com>
2020-09-15 18:14:08 +0800

    2020-09-15 afternoon

--- src/testdemo/TestDemo.java
+++ src/testdemo/TestDemo.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import java.io.*;
 import java.lang.annotation.*;
 import java.lang.reflect.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class TestDemo {
 
@@ -158,6 +160,83 @@ public class TestDemo {
         System.out.println(fieldAnnotation.columnName());
         System.out.println(fieldAnnotation.type());
         System.out.println(fieldAnnotation.length());
+    }
+
+    @Test
+    public void testScanner() {
+        Scanner scan = new Scanner(System.in);
+
+        // 1. next 方式接收字符串
+        System.out.println("next 方式接收: ");
+        // hasNext 方法会扫描缓冲区中是否有字符，有则返回 true 并继续扫描，否则将方法阻塞等待下次输入
+        while (!scan.hasNext("q")) {
+            System.out.println("输入的数据为: " + scan.next());
+        }
+
+        // 2. nextLine 方式接收字符串
+        System.out.println("nextLine 方式接收: ");
+        while (scan.hasNextLine()) {
+            System.out.println("输入的数据为: " + scan.nextLine());
+        }
+
+        scan.close();
+
+        /*
+        ||| next() 与 nextLine() 的区别 |||
+        - next():
+            1、一定要读取到有效字符后才可以结束输入。
+            2、对输入有效字符之前遇到的空白，next() 方法会自动将其去掉。
+            3、只有输入有效字符后才将其后面输入的空白作为分隔符或者结束符。
+            4、next() 不能得到带有空格的字符串。
+        - nextLine():
+            1、以Enter为结束符,也就是说 nextLine()方法返回的是输入回车之前的所有字符。
+            2、可以获得空白。
+         */
+    }
+
+    @Test
+    public void testStream() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c;
+        String str;
+
+        System.out.println("输入字符，以'q'结束: ");
+        do {
+            c = (char) br.read();
+            System.out.println(c);
+        } while (c != 'q');
+
+        System.out.println("\n输入字符串，以\"end\"结束: ");
+        do {
+            str = br.readLine();
+            System.out.println(str);
+        } while (!str.equals("end"));
+    }
+
+    @Test
+    public void testIoStream() throws IOException {
+        File f = new File("testText.txt");
+
+        FileOutputStream fop = new FileOutputStream(f);
+        // 构建 OutputStreamWriter 对象,参数可以指定编码,默认为操作系统默认编码, windows 上是 gbk
+        OutputStreamWriter writer = new OutputStreamWriter(fop, StandardCharsets.UTF_8);
+        // 写入到缓冲区
+        writer.append("中文输入");
+        writer.append("\r\n");
+        writer.append("English");
+        writer.close();    // 关闭写入流,同时会把缓冲区内容写入文件
+        fop.close();    // 关闭输出流,释放系统资源
+
+
+        FileInputStream fip = new FileInputStream(f);
+        InputStreamReader reader = new InputStreamReader(fip, StandardCharsets.UTF_8);
+        StringBuffer sb = new StringBuffer();
+        while (reader.ready()) {
+            sb.append((char) reader.read());
+        }
+        System.out.println(sb.toString());
+        reader.close();
+        fip.close();
     }
 }
 
