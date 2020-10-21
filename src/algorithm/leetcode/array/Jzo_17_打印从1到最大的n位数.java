cb87e0fce0c6a8f8d5b3aa603ac8a60ef3567038
echofzoe <echofzoe@gmail.com>
2020-10-21 14:06:06 +0800

    optimized some code

--- src/algorithm/leetcode/array/Jzo_17_打印从1到最大的n位数.java
+++ src/algorithm/leetcode/array/Jzo_17_打印从1到最大的n位数.java
@@ -11,7 +11,7 @@ public class Jzo_17_打印从1到最大的n位数 {
 
     public static void main(String[] args) {
         Jzo_17_打印从1到最大的n位数 lc = new Jzo_17_打印从1到最大的n位数();
-        int n = 1;
+        int n = 2;
 
         System.out.println("从1到最大的" + n + "位数的数组为: " + Arrays.toString(lc.printNumbers_BigNumberPrint(n)));
     }
@@ -29,34 +29,45 @@ public class Jzo_17_打印从1到最大的n位数 {
         return res;
     }
 
-    int nine = 0, count = 0, start, n;
-    int[] res;
-    char[] num, loop = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    int n;
+    int start;    // 非0数字起始位置
+    int nine = 0;    // 9的个数
+    int count = 0;    // 结果集记数位,结果集大小为 [1, 10^n]
+
+    char[] loop = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};    // 每一位可能的数字
+    char[] num;    // 当前数字的字符数组
+
+    int res[];    // 结果集
 
     public int[] printNumbers_BigNumberPrint(int n) {
+        // 特判
+        if (n == 0) return new int[0];
+
+        // 初始化
         this.n = n;
         res = new int[(int) Math.pow(10, n) - 1];
         num = new char[n];
         start = n - 1;
-        dfs(0);
+
+        dfs(0);    // 从第0位开始递归至第n位
 
         return res;
     }
 
     private void dfs(int x) {
-        if ((x == n)) {
+        if (x == n) {
             String s = String.valueOf(num).substring(start);
             if (!s.equals("0")) res[count++] = Integer.parseInt(s);
             if (n - start == nine) start--;
             return;
         }
+
         for (char c : loop) {
-            if (c == '9') {
-                nine++;
-            }
+            if (c == '9') nine++;
             num[x] = c;
             dfs(x + 1);
         }
+
         nine--;
     }
 }
--- src/algorithm/leetcode/bit/Jzo_15_二进制中1的个数.java
+++ src/algorithm/leetcode/bit/Jzo_15_二进制中1的个数.java
@@ -14,11 +14,12 @@ public class Jzo_15_二进制中1的个数 {
 
     public int hammingWeight(int n) {
         int count = 0;
-        for (int i = 0; i < 32; i++) {
+
+        while (n != 0) {
             if ((n & 1) == 1) {
                 count++;
             }
-            n = n >> 1;
+            n >>>= 1;
         }
 
         return count;
