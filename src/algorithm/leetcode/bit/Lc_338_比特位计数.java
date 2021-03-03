ecddbaaeb447ac3cf7382a128a08dc050993e987
echofzoe <echofzoe@gmail.com>
2021-03-03 09:50:25 +0800

    updated leetcode 338

---
+++ src/algorithm/leetcode/bit/Lc_338_比特位计数.java
@@ -0,0 +1,55 @@
+package algorithm.leetcode.bit;
+
+import java.util.Arrays;
+
+public class Lc_338_比特位计数 {
+
+    // 比特位计数
+    // https://leetcode-cn.com/problems/counting-bits/
+
+    public static void main(String[] args) {
+        Lc_338_比特位计数 lc = new Lc_338_比特位计数();
+        int num = 5;
+
+        System.out.println("对于[0," + num + "]范围内的每个数字，其二进制中1的数目的数组是" + Arrays.toString(lc.countBitsDp2(num)));
+    }
+
+    // BF - 时间复杂度 O(N * num) - 空间复杂度 O(1)
+    public int[] countBitsBf(int num) {
+        int[] res = new int[num + 1];
+
+        for (int i = 0; i <= num; i++) {
+            int tmp = i;
+            while (tmp > 0) {
+                tmp &= (tmp - 1);
+                res[i]++;
+            }
+        }
+
+        return res;
+    }
+    
+    // 动态规划 + 最低设置位 - 时间复杂度 O(num) - 空间复杂度 O(1)
+    public int[] countBitsDp1(int num) {
+        int[] res = new int[num + 1];
+
+        for (int i = 1; i <= num; i++) {
+            res[i] = res[i & (i - 1)] + 1;
+        }
+
+        return res;
+    }
+
+    // 动态规划 + 最低有效位 - 时间复杂度 O(num) - 空间复杂度 O(1)
+    // - 操作 i >> 1 会把 i 的最低位去掉
+    public int[] countBitsDp2(int num) {
+        int[] res = new int[num + 1];
+
+        for (int i = 1; i <= num; i++) {
+            res[i] = res[i >> 1] + (i & 1);
+        }
+
+        return res;
+    }
+
+}
