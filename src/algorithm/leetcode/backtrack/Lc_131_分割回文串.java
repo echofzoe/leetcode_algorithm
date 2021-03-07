ad33ba3daafd4c9a062f16efb7e97485c6ddc491
echofzoe_win <echofzoe@gmail.com>
2021-03-07 23:05:40 +0800

    updated leetcode 131

---
+++ src/algorithm/leetcode/backtrack/Lc_131_分割回文串.java
@@ -0,0 +1,21 @@
+package algorithm.leetcode.backtrack;
+
+import java.util.List;
+
+public class Lc_131_分割回文串 {
+
+    // 分割回文串
+    // https://leetcode-cn.com/problems/palindrome-partitioning/
+
+    public static void main(String[] args) {
+        Lc_131_分割回文串 lc = new Lc_131_分割回文串();
+        String s = "aab";
+
+        System.out.println("将\"" + s + "\"分割成一些子串，使每个子串都是回文串，所有的分割方案是" + lc.partition(s));
+    }
+
+    public List<List<String>> partition(String s) {
+        
+    }
+
+}
