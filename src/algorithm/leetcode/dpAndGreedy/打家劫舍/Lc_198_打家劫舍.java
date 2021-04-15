f4d2f8268121e56e9e9d4645c5f4f72348f9b2b3
echofzoe <echofzoe@gmail.com>
2021-04-15 15:01:22 +0800

    updated leetcode 213

--- src/algorithm/leetcode/dpAndGreedy/打家劫舍/Lc_198_打家劫舍.java
+++ src/algorithm/leetcode/dpAndGreedy/打家劫舍/Lc_198_打家劫舍.java
@@ -1,11 +1,15 @@
-package algorithm.leetcode.dpAndGreedy;
+package algorithm.leetcode.dpAndGreedy.打家劫舍;
 
 import java.util.Arrays;
 
+/**
+ * 打家劫舍
+ * https://leetcode-cn.com/problems/house-robber/
+ *
+ * @author echofzoe
+ * @since 2021.4.15
+ */
 public class Lc_198_打家劫舍 {
-
-    // 打家劫舍
-    // https://leetcode-cn.com/problems/house-robber/
 
     public static void main(String[] args) {
         Lc_198_打家劫舍 lc = new Lc_198_打家劫舍();
@@ -14,9 +18,10 @@ public class Lc_198_打家劫舍 {
         System.out.println("在房屋群" + Arrays.toString(input) + "中能偷窃到的最高金额是" + lc.robDp(input));
     }
 
-    // dp - 时间复杂度 O(N) - 空间复杂度 O(N) 可优化为O(1)
+    // DP - 时间复杂度 O(N) - 空间复杂度 O(N) 可优化为O(1)
+    // - dp[i]表示偷窃到第i间房子时所能得到的最高偷窃金额
+    // - 状态转移方程: dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1])
     public int robDp(int[] nums) {
-        if (nums == null) return 0;
         int n = nums.length;
         if (n == 0) return 0;
         if (n == 1) return nums[0];
@@ -27,7 +32,7 @@ public class Lc_198_打家劫舍 {
         dp[1] = Math.max(nums[0], nums[1]);
 
         for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
 
         return dp[n - 1];
@@ -35,7 +40,6 @@ public class Lc_198_打家劫舍 {
 
     // dp 优化 - 时间复杂度 O(N) - 空间复杂度 O(1)
     public int robDpOptimization(int[] nums) {
-        if (nums == null) return 0;
         int n = nums.length;
         if (n == 0) return 0;
         if (n == 1) return nums[0];
@@ -46,7 +50,7 @@ public class Lc_198_打家劫舍 {
         second = Math.max(nums[0], nums[1]);
 
         for (int i = 2; i < n; i++) {
-            int tmp = Math.max(second, first + nums[i]);
+            int tmp = Math.max(first + nums[i], second);
             first = second;
             second = tmp;
         }
---
+++ src/algorithm/leetcode/dpAndGreedy/打家劫舍/Lc_213_打家劫舍_II.java
@@ -0,0 +1,47 @@
+package algorithm.leetcode.dpAndGreedy.打家劫舍;
+
+import java.util.Arrays;
+
+/**
+ * 打家劫舍 II
+ * https://leetcode-cn.com/problems/house-robber-ii/description/
+ *
+ * @author echofzoe
+ * @date 2021.3.26
+ */
+public class Lc_213_打家劫舍_II {
+
+    public static void main(String[] args) {
+        Lc_213_打家劫舍_II lc = new Lc_213_打家劫舍_II();
+        int[] nums = {1, 2, 3, 1};
+
+        System.out.println("按照题意实施盗窃，能够偷窃到的最高金额是" + lc.rob(nums));
+    }
+
+    // DP - 时间复杂度 O(N) - 空间复杂度 O(N)
+    // - dp[i]表示偷窃到第i间房子时所能得到的最高偷窃金额
+    // - 状态转移方程: dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1])
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        if (n == 2) return Math.max(nums[0], nums[1]);
+
+        // 房间数量 >2 时，才能进行有效拆分计算
+        return Math.max(helper(nums, 0, n - 1), helper(nums, 1, n));
+    }
+
+    private int helper(int[] nums, int start, int end) {
+        int[] dp = new int[end];
+        // base case
+        dp[start] = nums[start];
+        dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+
+        for (int i = start + 2; i < end; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+
+        return dp[end - 1];
+    }
+
+}
