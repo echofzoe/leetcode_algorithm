c3afc164486d3ffce9f37b0fddb09fc3d9eb9b5c
echofzoe <echofzoe@gmail.com>
2021-04-23 11:14:06 +0800

    added leetcode 368

--- README.md
+++ README.md
@@ -7,3 +7,15 @@
 ### 项目依赖
 - *JDK14*
 - *JUnit4.12*
+
+---
+
+### 一些说明
+
+- 因为`MAVEN`的项目结构嵌套较为复杂，故本项目只使用了普通项目的架构。目前读者在使用时只需要手动引入`junit`的`jar`包即可
+- 因为笔者的代码中用到了一些`jdk14`的语法糖（如`switch`等），所以使用低版本`jdk`时会通不过编译。读者可以直接使用`jdk`高版本，或将语法糖改成`jdk8`支持的正常语法
+- 普通`idea`项目更改`jdk`编译版本：
+  1. `ctrl + alt + s`打开`Settings`面板，进入`Build,Execution,Depolyment -> Compiler -> Java Compiler`，将右侧`Module`下对应项目后的`Target bytecode version`改成你的`jdk`版本
+  2. `ctrl + alt + shift + s`打开`Project Structure`面板：
+     1. 进入`Project`，将右侧`Project SDK`和下面的`Project language level`改成你的`jdk`版本
+     2. 进入`Modules`，在右侧点击左键点击相应项目，再将右侧项目的`Dependencies`下的`Module SDK`改成你的`jdk`版本
---
+++ src/algorithm/leetcode/dpAndGreedy/Lc_368_最大整除子集.java
@@ -0,0 +1,70 @@
+package algorithm.leetcode.dpAndGreedy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 最大整除子集
+ * <P>https://leetcode-cn.com/problems/largest-divisible-subset/</P>
+ *
+ * @author echofzoe
+ * @since 2021.4.23
+ */
+public class Lc_368_最大整除子集 {
+
+    public static void main(String[] args) {
+        Lc_368_最大整除子集 lc = new Lc_368_最大整除子集();
+
+        int[] nums = {1, 2, 4, 8};
+
+        System.out.println("无重复的正整数数组" + Arrays.toString(nums) + "中的最大整除子集是" + lc.largestDivisibleSubset(nums));  // [8,4,2,1]
+    }
+
+    // DP - 时间复杂度 O() - 空间复杂度 O()
+    // - dp[i] 表示在升序数组里以nums[i]结尾最大整除子集的长度
+    // - 状态转移方程: {
+    //   dp[i] = max(dp[i], dp[j] + 1), j in [0,i)
+    // }
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        int[] dp = new int[n];
+        // base case
+        Arrays.fill(dp, 1);
+
+        int maxSize = 1, maxVal = 666;
+
+        // 1.动态规划找出最大子集的个数、最大子集中的最大整数
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+
+            if (dp[i] > maxSize) {
+                maxSize = dp[i];
+                maxVal = nums[i];
+            }
+        }
+
+        // 2.倒推获得最大子集
+        List<Integer> res = new ArrayList<>();
+        if (maxSize == 1) {
+            res.add(nums[0]);
+            return res;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (dp[i] == maxSize && maxVal % nums[i] == 0) {
+                res.add(nums[i]);
+                maxSize--;
+                maxVal = nums[i];
+            }
+        }
+
+        return res;
+    }
+
+}
