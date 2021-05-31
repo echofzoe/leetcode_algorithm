858a514bc7173812e85dcfe9b4bacc5443a2ca86
echofzoe <echofzoe@gmail.com>
2021-05-31 14:24:23 +0800

    updated leetcode 1878,5756

--- src/algorithm/leetcode/双周赛/lc_2021_5_29_图森未来/Lc_1878_矩阵中最大的三个菱形和.java
+++ src/algorithm/leetcode/双周赛/lc_2021_5_29_图森未来/Lc_1878_矩阵中最大的三个菱形和.java
@@ -1,6 +1,6 @@
 package algorithm.leetcode.双周赛.lc_2021_5_29_图森未来;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * 矩阵中最大的三个菱形和
@@ -15,14 +15,58 @@ public class Lc_1878_矩阵中最大的三个菱形和 {
         Lc_1878_矩阵中最大的三个菱形和 lc = new Lc_1878_矩阵中最大的三个菱形和();
 
         int[][] grid = {{3, 4, 5, 1, 3}, {3, 3, 4, 2, 3}, {20, 30, 200, 40, 10}, {1, 5, 5, 4, 1}, {4, 3, 2, 2, 5}};
+        int[][] grid1 = {{7, 7, 7}};
 
         System.out.println("输入：grid = " + Arrays.deepToString(grid));
         System.out.println("输出：" + Arrays.toString(lc.getBiggestThree(grid)));
+        System.out.println("输入：grid1 = " + Arrays.deepToString(grid1));
+        System.out.println("输出：" + Arrays.toString(lc.getBiggestThree(grid1)));
 
     }
 
     public int[] getBiggestThree(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
 
+        Queue<Integer> cand = new PriorityQueue<>((a, b) -> b - a);
+        Set<Integer> set = new HashSet<>();
+        int[][] dirs = {{1, -1}, {1, 1}, {-1, 1}, {-1, -1}};
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // 以 grid[i][j] 为中心
+                if (set.add(grid[i][j])) {
+                    cand.offer(grid[i][j]);
+                }
+
+                // 枚举菱形的边长
+                for (int k = 1; k <= Math.min(m, n); k++) {
+                    if (i - k < 0 || i + k >= m || j - k < 0 || j + k >= n) break;
+
+                    // 从最高点开始遍历菱形的4条边
+                    int x = i - k, y = j;
+                    int sum = 0;
+                    for (int c = 0; c < 4; c++) {
+                        int step = k;
+                        while (step > 0) {
+                            x += dirs[c][0];
+                            y += dirs[c][1];
+                            sum += grid[x][y];
+                            step--;
+                        }
+                    }
+
+                    if (set.add(sum)) {
+                        cand.offer(sum);
+                    }
+                }
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 3 && !cand.isEmpty(); i++) list.add(cand.poll());
+
+        // 先将 List<Integer> 转成 IntStream，再转成 int[]
+        return list.stream().mapToInt(Integer::valueOf).toArray();
     }
 
 }
--- src/algorithm/leetcode/双周赛/lc_2021_5_29_图森未来/Lc_5756_两个数组最小的异或值之和.java
+++ src/algorithm/leetcode/双周赛/lc_2021_5_29_图森未来/Lc_5756_两个数组最小的异或值之和.java
@@ -15,7 +15,7 @@ public class Lc_5756_两个数组最小的异或值之和 {
         Lc_5756_两个数组最小的异或值之和 lc = new Lc_5756_两个数组最小的异或值之和();
 
         int[] nums1 = {1, 0, 3}, nums2 = {5, 3, 4};
-        
+
         System.out.println("给你两个整数数组 nums1 和 nums2 ，它们长度都为 n 。\n" +
                 "两个数组的 异或值之和 为 (nums1[0] XOR nums2[0]) + (nums1[1] XOR nums2[1]) + ... + (nums1[n - 1] XOR nums2[n - 1]) （下标从 0 开始）。\n" +
                 "  - 比方说，[1,2,3] 和 [3,2,1] 的 异或值之和 等于 (1 XOR 3) + (2 XOR 2) + (3 XOR 1) = 2 + 0 + 2 = 4 。\n" +
@@ -25,8 +25,82 @@ public class Lc_5756_两个数组最小的异或值之和 {
         System.out.println("输出：" + lc.minimumXORSum(nums1, nums2));
     }
 
+    // 状态压缩DP - 时间复杂度 O(N * 2^N) - 空间复杂度 O(2^N) 为状态的数量
     public int minimumXORSum(int[] nums1, int[] nums2) {
+        int n = nums1.length;
 
+        // 使用长度为n的二进制数mask表示数组nums2中的数被选择的状态
+        // 如果mask从低到高的第i位为1，则说明nums2[i]已经被选择，否则说明其未被选择
+        int range = 1 << n;
+
+        // dp[mask] 表示当我们选择了nums2中的元素的状态为mask，并且选择了nums1的前count(mask)个元素的情况下，可以组成的最小异或值之和
+        /*
+            - 状态转移方程: {
+                dp[mask] = min(dp[mask], dp[mask ^ (1 << i)] + (nums1[Integer.bitCount(mask) - 1] ^ nums2[i])), 0 < i < n
+            }
+        */
+        int[] dp = new int[range];
+        // base case
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int mask = 1; mask < range; mask++) {
+            // 枚举 nums2 的每一位选择
+            for (int i = 0; i < n; i++) {
+                // 判断 mask 的第 i 位是否为 1
+                if ((mask & (1 << i)) > 0) {
+                    // 当 mask 的第 i 位是 1 时，操作 mask ^ (1 << i) 会将 mask 的第 i 位置 0
+                    dp[mask] = Math.min(dp[mask], dp[mask ^ (1 << i)] + (nums1[Integer.bitCount(mask) - 1] ^ nums2[i]));
+                }
+            }
+        }
+
+        return dp[range - 1];
+    }
+
+    // 回溯（超时） - 时间复杂度 O() - 空间复杂度 O()
+    int n, res;
+
+    public int minimumXORSum1(int[] nums1, int[] nums2) {
+        this.n = nums1.length;
+        this.res = Integer.MAX_VALUE;
+
+        dfs(nums1, nums2, 0, 0);
+
+        return res;
+    }
+
+    private void dfs(int[] nums1, int[] nums2, int j, int tmp) {
+        // exit
+        if (j == n - 1) {
+            tmp += nums1[j] ^ nums2[j];
+            res = Math.min(res, tmp);
+            return;
+        }
+
+        // dfs
+        for (int i = j; i < n; i++) {
+            swap(nums2, j, i);
+            tmp += nums1[j] ^ nums2[j];
+
+            // 剪枝
+            if (tmp >= res) {
+                tmp -= nums1[j] ^ nums2[j];
+                swap(nums2, j, i);
+                continue;
+            }
+
+            dfs(nums1, nums2, j + 1, tmp);
+            // 回溯
+            tmp -= nums1[j] ^ nums2[j];
+            swap(nums2, j, i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 
 }
