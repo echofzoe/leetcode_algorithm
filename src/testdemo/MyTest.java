7aa08284a5bd6527fc4b4789b890463871120776
echofzoe <echofzoe@gmail.com>
2021-06-02 17:58:40 +0800

    updated something

--- src/testdemo/MyTest.java
+++ src/testdemo/MyTest.java
@@ -71,7 +71,91 @@ public class MyTest {
 
     @Test
     public void test5() {
+        byte stepDrop = 4;
+        Point start = new Point(0, 0);
+        Point end = new Point(1, 1);
+        List<List<Point>> paths = findPath(map, start, end, stepDrop);
+        for (List<Point> path : paths) {
+            System.out.println(path);
+        }
+    }
 
+    /**
+     * 查找最优路径
+     *
+     * @param map      地形数据
+     * @param start    起始点
+     * @param end      结束点
+     * @param stepDrop 单步落差
+     * @return
+     */
+    public static List<List<Point>> findPath(byte[][] map, Point start, Point end, byte stepDrop) {
+        m = map.length;
+        n = map[0].length;
+        memo = new boolean[m][n];
+        res = new LinkedList<>();
+        tmp = new LinkedList<>();
+
+        dfs(start, end, 0);
+
+        return res;
+    }
+
+    private static final int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private static boolean[][] memo;
+    private static List<List<Point>> res;
+    private static List<Point> tmp;
+    private static int m, n;
+
+    private static void dfs(Point cur, Point end, int idx) {
+        int x = cur.x, y = cur.y;
+        if (x == end.x && y == end.y) {
+            tmp.add(end);
+            res.add(new ArrayList<>(tmp));
+            tmp.remove(tmp.size() - 1);
+            return;
+        }
+
+        if (x < 0 || y < 0 || x >= m || y >= n || memo[x][y]) return;
+
+        memo[x][y] = true;
+        tmp.add(cur);
+
+        for (int[] dir : dirs) {
+            int x1 = x + dir[0], y1 = y + dir[1];
+            dfs(new Point(x1, y1), end, idx + 1);
+        }
+
+        // backtrace
+        tmp.remove(idx);
+        memo[x][y] = false;
+    }
+
+    static byte[][] map =
+            {
+//                    {11, 12, 15, 18, 20},
+//                    {16, 20, 20, 20, 21},
+//                    {15, 20, 29, 20, 23},
+//                    {14, 20, 20, 20, 24},
+//                    {13, 20, 25, 20, 29}
+                    {11, 12, 15},
+                    {16, 20, 20},
+                    {15, 20, 29}
+            };
+
+    static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + "," + y + ")";
+        }
     }
 
 }
