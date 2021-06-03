dabae0c0e5352e17169a277d1e0c61c07e668c83
echofzoe_win <echofzoe@gmail.com>
2021-06-03 22:52:38 +0800

    updated leetcode 131

--- src/algorithm/leetcode/backtrack/Lc_131_分割回文串.java
+++ src/algorithm/leetcode/backtrack/Lc_131_分割回文串.java
@@ -3,108 +3,129 @@ package algorithm.leetcode.backtrack;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 分割回文串
+ * <P>https://leetcode-cn.com/problems/palindrome-partitioning/</P>
+ *
+ * @author echofzoe
+ * @since unknown
+ * @updated 2021.6.3
+ */
 public class Lc_131_分割回文串 {
-
-    // 分割回文串
-    // https://leetcode-cn.com/problems/palindrome-partitioning/
 
     public static void main(String[] args) {
         Lc_131_分割回文串 lc = new Lc_131_分割回文串();
+
         String s = "aab";
 
-        System.out.println("将\"" + s + "\"分割成一些子串，使每个子串都是回文串，所有的分割方案是" + lc.partition2(s));
+        System.out.println("将\"" + s + "\"分割成一些子串，使每个子串都是回文串，所有的分割方案是" + lc.partition1(s));  // [[a, a, b], [aa, b]]
     }
 
     // 回溯 + DP - 时间复杂度 O(N * 2^N) - 空间复杂度 O(N^2)
-    public List<List<String>> partition1(String s) {
-        int n = s.length();
+    int n;
+    String s;
+    List<List<String>> res;
+    List<String> cur;
+    boolean[][] dp;
 
-        // DP 预处理回文串
-        char[] cs = s.toCharArray();
-        // f[i][j] 表示 s.substring(i, j+1) 是否为回文串
-        boolean[][] f = new boolean[n][n];
+    public List<List<String>> partition(String s) {
+        n = s.length();
+        this.s = s;
+        res = new ArrayList<>();
+        cur = new ArrayList<>();
+
+        // dp[i][j] 表示 s.substring(i, j + 1) 是否是回文串
+        dp = new boolean[n][n];
+
+        // dp 预处理源字符串
         for (int j = 0; j < n; j++) {
             for (int i = 0; i <= j; i++) {
                 if (i == j) {
-                    f[i][j] = true;
+                    dp[i][j] = true;
                 } else {
+                    boolean b = s.charAt(i) == s.charAt(j);
                     if (j - i + 1 == 2) {
-                        // 当 [i,j] 长度为 2 时
-                        f[i][j] = cs[i] == cs[j];
+                        dp[i][j] = b;
                     } else {
-                        // 当 [i,j] 长度大于 2 时
-                        f[i][j] = cs[i] == cs[j] && f[i + 1][j - 1];
+                        dp[i][j] = b && dp[i + 1][j - 1];
                     }
                 }
             }
         }
 
-        List<List<String>> res = new ArrayList<>();
-        List<String> cur = new ArrayList<>();
+        dfs(0);
 
-        dfs1(res, cur, s, 0, f);
         return res;
     }
 
-    private void dfs1(List<List<String>> res, List<String> cur, String s, int lo, boolean[][] f) {
-        int n = s.length();
-        if (lo == n) {
+    private void dfs(int i) {
+        if (i == n) {
+            // exit
             res.add(new ArrayList<>(cur));
             return;
         }
 
-        for (int hi = lo; hi < n; hi++) {
-            if (f[lo][hi]) {
-                cur.add(s.substring(lo, hi + 1));
-                dfs1(res, cur, s, hi + 1, f);
+        for (int j = i; j < n; j++) {
+            if (dp[i][j]) {
+                // record
+                cur.add(s.substring(i, j + 1));
+
+                // dfs
+                dfs(j + 1);
+
+                // backtrace
                 cur.remove(cur.size() - 1);
             }
         }
     }
 
     // 回溯 + 记忆化搜索 - 时间复杂度 O(N * 2^N) - 空间复杂度 O(N^2)
-    public List<List<String>> partition2(String s) {
-        int n = s.length();
+    int[][] mem;
 
-        int[][] mem = new int[n][n];
+    public List<List<String>> partition1(String s) {
+        n = s.length();
+        this.s = s;
+        res = new ArrayList<>();
+        cur = new ArrayList<>();
+        mem = new int[n][n];
 
-        List<List<String>> res = new ArrayList<>();
-        List<String> cur = new ArrayList<>();
+        dfs1(0);
 
-        dfs2(res, cur, s, 0, mem);
         return res;
     }
 
-    // 记忆化搜索
-    private void dfs2(List<List<String>> res, List<String> cur, String s, int lo, int[][] mem) {
-        int n = s.length();
-        if (lo == n) {
+    private void dfs1(int i) {
+        if (i == n) {
+            // exit
             res.add(new ArrayList<>(cur));
             return;
         }
 
-        for (int hi = lo; hi < n; hi++) {
-            if (checkPalindrome(s, lo, hi, mem) == 1) {
-                cur.add(s.substring(lo, hi + 1));
-                dfs2(res, cur, s, hi + 1, mem);
+        for (int j = i; j < n; j++) {
+            if (check(i, j) == 1) {
+                // record
+                cur.add(s.substring(i, j + 1));
+
+                // dfs
+                dfs1(j + 1);
+
+                // backtrace
                 cur.remove(cur.size() - 1);
             }
         }
     }
 
     // 在遍历回文串的同时设置记忆化数组，记忆化数组 mem[i][j] 值为0表示未搜索，为1表示是回文串，为-1表示不是回文串
-    private int checkPalindrome(String s, int lo, int hi, int[][] mem) {
-        if (mem[lo][hi] != 0) return mem[lo][hi];
+    private int check(int i, int j) {
+        if (mem[i][j] != 0) return mem[i][j];
 
-        if (lo == hi) {
-            mem[lo][hi] = 1;
-        } else if (hi - lo + 1 == 2) {
-            mem[lo][hi] = s.charAt(lo) == s.charAt(hi) ? 1 : -1;
-        } else {
-            mem[lo][hi] = s.charAt(lo) == s.charAt(hi) ? checkPalindrome(s, lo + 1, hi - 1, mem) : -1;
-        }
+        boolean b = s.charAt(i) == s.charAt(j);
 
-        return mem[lo][hi];
+        if (i == j) mem[i][j] = 1;
+        else if (j - i + 1 == 2) mem[i][j] = b ? 1 : -1;
+        else mem[i][j] = b ? check(i + 1, j - 1) : -1;
+
+        return mem[i][j];
     }
 
 }
