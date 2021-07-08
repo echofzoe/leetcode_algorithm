967e37ff1889e8273a4d84e93eae61210a6e34f4
echofzoe <echofzoe@gmail.com>
2021-07-08 10:00:04 +0800

    updated leetcode 213

--- src/algorithm/leetcode/dpAndGreedy/打家劫舍/Lc_213_打家劫舍_II.java
+++ src/algorithm/leetcode/dpAndGreedy/打家劫舍/Lc_213_打家劫舍_II.java
@@ -4,18 +4,23 @@ import java.util.Arrays;
 
 /**
  * 打家劫舍 II
- * https://leetcode-cn.com/problems/house-robber-ii/description/
+ * <P>https://leetcode-cn.com/problems/house-robber-ii/description/</P>
  *
  * @author echofzoe
- * @date 2021.3.26
+ * @since 2021.3.26
  */
 public class Lc_213_打家劫舍_II {
 
     public static void main(String[] args) {
         Lc_213_打家劫舍_II lc = new Lc_213_打家劫舍_II();
-        int[] nums = {1, 2, 3, 1};
 
-        System.out.println("按照题意实施盗窃，能够偷窃到的最高金额是" + lc.rob(nums));
+        int[] nums = {1, 3, 1, 3, 100};
+
+        System.out.println("你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。\n" +
+                "同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。\n" +
+                "给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。\n");
+        System.out.println("输入：nums = " + Arrays.toString(nums));
+        System.out.println("输出：" + lc.rob(nums));  // 103
     }
 
     // DP - 时间复杂度 O(N) - 空间复杂度 O(N)
@@ -25,25 +30,26 @@ public class Lc_213_打家劫舍_II {
     // }
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 0) return 0;
+
         if (n == 1) return nums[0];
         if (n == 2) return Math.max(nums[0], nums[1]);
 
-        // 房间数量 >2 时，才能进行有效拆分计算
-        return Math.max(helper(nums, 0, n - 1), helper(nums, 1, n));
+        return Math.max(helper(nums, 0, n - 2), helper(nums, 1, n - 1));
     }
 
-    private int helper(int[] nums, int start, int end) {
-        int[] dp = new int[end];
-        // base case
-        dp[start] = nums[start];
-        dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+    private int helper(int[] nums, int l, int r) {
+        int n = r - l + 1;
+        if (n == 1) return nums[l];
+        if (n == 2) return Math.max(nums[l], nums[l + 1]);
 
-        for (int i = start + 2; i < end; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        int a = nums[l], b = Math.max(nums[l], nums[l + 1]), c;
+        for (int i = l + 2; i <= r; i++) {
+            c = Math.max(a + nums[i], b);
+            a = b;
+            b = c;
         }
 
-        return dp[end - 1];
+        return b;
     }
 
 }
