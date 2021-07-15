989f7e749328b52060b2354ac58aa33d4c3b2359
echofzoe <echofzoe@gmail.com>
2021-07-15 09:39:49 +0800

    updated jian zhi offer 33

--- src/algorithm/剑指offer/tree/Jzo_33_二叉搜索树的后序遍历序列.java
+++ src/algorithm/剑指offer/tree/Jzo_33_二叉搜索树的后序遍历序列.java
@@ -1,27 +1,29 @@
 package algorithm.剑指offer.tree;
 
-import algorithm.leetcode.utils.BinaryTreeSerialize;
-import algorithm.leetcode.utils.TreeNode;
-
 import java.util.Arrays;
 
+/**
+ * 二叉搜索树的后序遍历序列
+ * <P>https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof/</P>
+ *
+ * @author echofzoe
+ * @since unknown
+ * @updated 2021.7.15
+ */
 public class Jzo_33_二叉搜索树的后序遍历序列 {
-
-    // 二叉搜索树的后序遍历序列
-    // https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof/
 
     public static void main(String[] args) {
         Jzo_33_二叉搜索树的后序遍历序列 lc = new Jzo_33_二叉搜索树的后序遍历序列();
-        TreeNode root = new TreeNode(0);
-        lc.treeInitialize(root);
 
-        int[] postorder1 = new int[] {1,6,3,2,5};
-        int[] postorder2 = new int[] {1,3,2,6,5};
+        int[] postorder = new int[] {1,6,3,2,5};
 
-        System.out.print("整数数组" + Arrays.toString(postorder2) + (lc.verifyPostorder_Recursion(postorder2) ? "是" : "不是") + "二叉搜索树" + BinaryTreeSerialize.serialize(root) + "的后序遍历结果");
+        System.out.println("输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回 true，否则返回 false。假设输入的数组的任意两个数字都互不相同。");
+        System.out.println("输入：postorder = " + Arrays.toString(postorder));
+        System.out.println("输出：" + lc.verifyPostorder(postorder));
     }
 
-    public boolean verifyPostorder_Recursion(int[] postorder) {
+    // 递归 - 时间复杂度 O(N^2) - 空间复杂度 O(N) 最差情况下，树退化成链表，递归深度达到N
+    public boolean verifyPostorder(int[] postorder) {
         return recur(postorder, 0, postorder.length - 1);
     }
 
@@ -29,31 +31,14 @@ public class Jzo_33_二叉搜索树的后序遍历序列 {
         if (i >= j) return true;
 
         int cur = i;
-
         while (postorder[cur] < postorder[j]) cur++;
-
-        int leftSubtreeMaxIndex = cur - 1;    // 确定左子树的右边界
+        int leftSubtreeMaxIndex = cur - 1;  // 确定左子树的索引右边界
 
         while (postorder[cur] > postorder[j]) cur++;
-
-        // return cur == j && recur(postorder, i, leftSubtreeMaxIndex) && recur(postorder, leftSubtreeMaxIndex + 1, j - 1);
-
-        int rightSubtreeMaxIndex = cur - 1;
+        int rightSubtreeMaxIndex = cur - 1;  // 确定右子树的索引右边界
 
         return rightSubtreeMaxIndex + 1 == j && recur(postorder, i, leftSubtreeMaxIndex)
                 && recur(postorder, leftSubtreeMaxIndex + 1, rightSubtreeMaxIndex);
     }
 
-    private void treeInitialize(TreeNode root) {
-        // depth = 1
-        root.val = 5;
-
-        // depth = 2
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(6);
-
-        // depth = 3
-        root.left.left = new TreeNode(1);
-        root.left.right = new TreeNode(3);
-    }
 }
